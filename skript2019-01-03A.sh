1643637b6df135c528aba0a0f60ff16ac49b25e6
filipremahl <46345405+filipremahl@users.noreply.github.com>
2019-01-06 02:13:43 +0100

    Update skript2019-01-03A.sh

--- skript2019-01-03A.sh
+++ skript2019-01-03A.sh
@@ -5,6 +5,6 @@ mkdir /home/filip/backup
 
 for i in $var; do
   file=${i##*/}
-  mv $i /home/filip/backup/$file
+  cp $i /home/filip/backup/$file
 done
 tar -cvzf /home/filip/backup /home/filip/"$DATE".tar.gz
